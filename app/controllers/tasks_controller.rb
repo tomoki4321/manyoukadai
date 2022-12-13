0529838db944c110c720171977f84c10f8263a55
tomoki <benzousan7@gmail.com>
2022-12-13 17:48:29 +0900

    タスクの新規作成画面を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,23 @@ class TasksController < ApplicationController
   def show
     @task=Task.find[params[:id]]
   end
+
+  def new
+    @task=Task.new
+  end
+
+  def create
+    @task=Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを作成しました！"
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def task_params
+    params.require(:task).permit(:task_name,:task_content)
+  end
 end
