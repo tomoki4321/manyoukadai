67c22e369fa96b581b818ad74b7f831178b809d2
tomoki <benzousan7@gmail.com>
2022-12-14 10:49:02 +0900

    一覧画面を降順に表示されるように設定

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module ManyouKadaiApp
     config.load_defaults 6.0
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
 
     config.generators do |g|
       g.test_framework :rspec,
