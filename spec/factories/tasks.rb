d92527d1e55969c0818d1f1ea23f74680b928fbf
tomoki <benzousan7@gmail.com>
2022-12-15 11:27:14 +0900

    tasks.rbにlimitデータを追加、System Specにテストコードを追記し、通過を確認

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,10 +2,12 @@ FactoryBot.define do
   factory :task do
     task_name { 'タスクネーム１' }
     task_content { 'タスク内容１' }
+    limit {'2022/12/15'}
   end
 
   factory :second_task, class: Task do
     task_name { 'タスクネーム２' }
     task_content { 'タスク内容２' }
+    limit {'2022/12/14'}
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -19,13 +19,25 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(task_list.first).to have_content 'タスクネーム２'
       end
     end
+    context 'タスクが終了期限の昇順に並んでいる場合' do
+      it '終了期限が早いタスクが一番上に表示される' do
+        @task1 = task
+        @task2 = task2
+        visit tasks_path
+        click_link "終了期限でソートする"
+        task_list = all('.task_all')
+        expect(task_list.first).to have_content 'タスクネーム２'
+      end
+    end
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
+        date = Date.new(2022,12,15)
         visit new_task_path
         fill_in 'task[task_name]', with: 'テスト１'
         fill_in 'task[task_content]', with: 'テスト２'
+        fill_in "task[limit]", with: date
         click_button "タスク作成"
         expect(page).to have_content 'テスト１'
       end
