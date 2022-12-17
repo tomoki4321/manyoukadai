7d68105ee0dc7166fd43d590a9295991d7b4d2b1
tomoki <benzousan7@gmail.com>
2022-12-17 17:52:29 +0900

    ログイン・ログアウト機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root "tasks#index"
+  root "sessions#new"
   resources :tasks do
     collection do
       get 'search'
     end
   end
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
