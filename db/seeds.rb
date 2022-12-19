35bb42b71edad743123bd153321a135f082ae9bc
tomoki <benzousan7@gmail.com>
2022-12-19 20:59:07 +0900

    seedデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,14 @@ User.create!(name: "adminuser",
   admin: true
 )
 
-User.create!(name: "user",
-email: "user@gmail.com",
-password: "111111",
-admin: false
-)
+10.times{|n|
+  User.create!(
+  name: "users"+n.to_s,
+  email: "users@"+n.to_s+".com",
+  password: "user"+(n.to_s)*4,
+  admin: false
+  )
+}
 labels = [
   {id: 1, label_name: "日本"},
   {id: 2, label_name: "アメリカ"},
@@ -24,3 +27,30 @@ labels = [
 labels.each do |label|
   Label.find_or_create_by(label)
 end
+
+start_day = Date.new(2020,1,1)
+last_day = Date.new(2022,12,31)
+users = User.all
+
+users = users.map{|user|user.id}
+
+100.times{|n|
+  Task.create!(
+    task_name: 'タスク名だよ'+n.to_s,
+    task_detail: 'タスク内容'+n.to_s,
+    expiry_date: rand(start_day..last_day),
+    status: rand(1..3),
+    priority: rand(1..3),
+    user_id: users.sample
+  )
+}
+
+tasks = Task.all
+tasks = tasks.map{|task| task.id}
+
+100.times{|n|
+  Labelling.create(
+    label_id: rand(1..10),
+    task_id: tasks.sample
+  )
+}
